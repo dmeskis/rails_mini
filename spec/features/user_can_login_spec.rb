d4046748dbef3472b3c0c0d527c5838dd0d3e08d
dmeskis <dmeskis@gmail.com>
2018-09-01 13:18:55 -0600

    User can log in

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'User can log in' do
   describe 'on the landing page' do
     it 'lets user click login if they already have an account' do
-      user = User.create(username: "Vagrant", password: "password")
+      user = User.create!(username: "Vagrant", email: "email@mail.com", password: "password")
 
       visit '/'
 
@@ -12,14 +12,13 @@ describe 'User can log in' do
       expect(current_path).to eq(login_path)
 
       fill_in "username", with: user.username
-      fill_in "username", with: user.password
+      fill_in "password", with: user.password
 
-      click_on "Log in"
+      click_on "Log In"
 
       expect(current_path).to eq(user_path(user))
 
       expect(page).to have_content("Welcome, #{user.username}")
-      expect(page).to have_content("Log out")
     end
   end
 end
