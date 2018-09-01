fee280fda3680d7940b18a8461ad2b2cf6bea80f
dmeskis <dmeskis@gmail.com>
2018-09-01 13:50:48 -0600

    Added role tests to model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,21 @@ describe User, type: :model do
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email)}
   end
+  describe "roles" do
+    it "can be created as a teacher" do
+      user = User.create(username: "teacher",
+                         password: "password",
+                         email: "email@mail.com",
+                         role: 1)
+      expect(user.role).to eq("teacher")
+      expect(user.teacher?).to be_truthy
+    end
+    it "can be created as a parent" do
+      user = User.create(username: "parent",
+                         password: "pass",
+                         role: 0)
+      expect(user.role).to eq("parent")
+      expect(user.parent?).to be_truthy
+    end
+  end
 end
