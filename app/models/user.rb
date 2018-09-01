a17376ad3bee69da268d39470a38d46de1a22243
dmeskis <dmeskis@gmail.com>
2018-08-31 19:15:02 -0600

    Installed BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password, require: true
+  validates_presence_of :email, :role
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180901010501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.integer "role"
+    t.string "email"
+    t.string "phone"
+    t.string "image"
+  end
 
 end
