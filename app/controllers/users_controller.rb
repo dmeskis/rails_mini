ee4bdfe61d0cac773c3ea71cb87d6d7d645956d5
dmeskis <dmeskis@gmail.com>
2018-09-04 17:27:53 -0600

    Stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    params_no_empty_fields = user_params.reject! {|k, v| v == ""}
+    @user = User.find(params[:id])
+    if @user.update(params_no_empty_fields)
+      flash[:success] = "Profile succesfully updated!"
+      redirect_to user_path(@user)
+    else
+      flash[:failure] = "Something went wrong, please try again."
+      render :edit
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -22,6 +38,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:username, :password, :email, :first_name, :last_name)
+      params.require(:user).permit(:username, :password, :email, :phone, :first_name, :last_name, :image)
     end
 end
--- spec/features/user_can_edit_profile_spec.rb
+++ spec/features/user_can_edit_profile_spec.rb
@@ -11,8 +11,8 @@ describe 'User can edit profile' do
 
       expect(current_path).to eq(login_path)
 
-      fill_in "username", with: teacher.username
-      fill_in "password", with: teacher.password
+      fill_in "username", with: user.username
+      fill_in "password", with: user.password
 
       within(".card-body") do
         click_on "Log In"
@@ -21,12 +21,13 @@ describe 'User can edit profile' do
       visit edit_user_path(user)
 
       fill_in :user_password, with: "new_password"
-      fill_in :user_email, with: "new_email@mail.com"
       fill_in :user_first_name, with: "Bob"
       fill_in :user_last_name, with: "Saget"
+      fill_in :user_phone, with: "815 260 4105"
       click_on "Confirm Changes"
 
-      expect(current_path).to eq(dashboard_path)
+      expect(current_path).to eq(user_path(user))
+      save_and_open_page
       expect(page).to have_content("Bob")
       expect(page).to have_content("Saget")
     end
