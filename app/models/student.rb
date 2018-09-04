98fd4bb16fffe9bb68ae7fbd89b5fe6d7d80e8a6
dmeskis <dmeskis@gmail.com>
2018-09-04 14:42:31 -0600

    Teacher can upload student image when creating

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,13 @@ class Student < ApplicationRecord
   has_many :students_users
   has_many :users, through: :students_users
   has_many :forms
+  
+  mount_uploader :image, ImageUploader
+  validates_processing_of :image
+  validate :image_size_validation
+
+  private
+    def image_size_validation
+      errors[:image] << "should be less than 500KB" if image.size > 0.5.megabytes
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   has_many :students_users
   has_many :students, through: :students_users
   has_many :forms, through: :students
+
+  mount_uploader :image, ImageUploader
+  validates_processing_of :image
+  validate :image_size_validation
+
+  private
+    def image_size_validation
+      errors[:image] << "should be less than 500KB" if image.size > 0.5.megabytes
+    end
 end
