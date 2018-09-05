e2315686b4c987a41ab899fcaaad8187f9c9545e
dmeskis <dmeskis@gmail.com>
2018-09-05 10:34:10 -0600

    Wrote test for teacher updating student guardian relations

--- spec/features/teacher_can_update_student_guardian_relations_spec.rb
+++ spec/features/teacher_can_update_student_guardian_relations_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-describe 'a teacher can edit a student' do
+describe 'a teacher can view a single student' do
   describe 'from their index page' do
-    it 'clicks edit student and they can edit that student' do
+    it 'clicks a student and they can view that student' do
       teacher = create(:user)
       student = teacher.students.create!(first_name: "Billy", last_name: "Madison")
       form = student.forms.create!(mood: "Happy",
@@ -10,6 +10,13 @@ describe 'a teacher can edit a student' do
                                   done_better_at: "Things",
                                   favorite_part: "Lunch",
                                   confirmed: 0)
+      guardian = User.create!(first_name: "Bill",
+                              last_name: "O'Reilly",
+                              phone: "555 555 5555",
+                              email: "bill@mail.com",
+                              username: "billy",
+                              password: "password",
+                              role: 0)
      visit '/'
 
      click_on "Log In"
@@ -28,10 +35,11 @@ describe 'a teacher can edit a student' do
      click_on "Edit Student"
      expect(current_path).to eq(edit_teacher_student_path(student))
 
-     fill_in :student_first_name, with: "Jesus"
+     select "Bill O'Reilly", :from => "guardianSelect"
+
      click_on "Confirm Changes"
      expect(current_path).to eq(teacher_student_path(student))
-     expect(page).to have_content("Jesus")
+     expect(page).to have_content("#{guardian.first_name}, #{guardian.last_name}")
     end
   end
 end
